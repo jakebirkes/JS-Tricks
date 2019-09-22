5aaf11b6cc381bb3f9d20ba325ab85539f0f60e9
Jake Wantulok <wantulok.jake@gmail.com>
2019-09-22 14:36:28 -0700

    groupArray into proper function

--- groupArray.js
+++ groupArray.js
@@ -1,22 +1,18 @@
 //this allows to group the array by any number we wish
-Object.defineProperty(Array.prototype, 'groupArray', {
-    value: function (groupSize) {
-      var array = this;
-      return [].concat.apply([],
-        array.map(function (elem, i) {
+function groupArray (array, groupSize) {
+  return [].concat.apply([],
+      array.map(function (elem, i) {
           return i % groupSize ? [] : [array.slice(i, i + groupSize)];
-        })
-      );
-    }
-  });
+      })
+  );
+}
 
 /*
 
 Example:
 
   let thisArray = [0, 1, 2, 3, 4, 5, 6, 7];
-  let newArrayGroups = thisArray.groupArray(2); //grouping them in pairs
-  console.log(newArrayGroups);
+  groupArray(thisArray, 2); //grouping them in pairs
 
 Result:
   >(4) [Array(2), Array(2), Array(2), Array(2)]
